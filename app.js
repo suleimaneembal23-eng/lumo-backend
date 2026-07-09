3512d35f341e41da1c3246b3b4b2ae11556eec65
Lumo Dev <lumobissau@gmail.com>
2026-07-09 02:04:08 +0100

    Fix CORS policy to allow Netlify and use node for production start

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 // 🔹 CORS configurado
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: ["http://localhost:3000", "http://localhost:5173", "https://lumobissau.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
