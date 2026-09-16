cd7f6b392045ae51c7a1d58034fc8e2f4226ae78
Lumo Dev <lumobissau@gmail.com>
2026-09-16 19:19:49 +0100

    Allow lumobissau.com and www.lumobissau.com in CORS

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 // 🔹 CORS configurado
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173", "https://lumobissau.netlify.app"],
+    origin: ["http://localhost:3000", "http://localhost:5173", "https://lumobissau.netlify.app", "https://lumobissau.com", "https://www.lumobissau.com"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
